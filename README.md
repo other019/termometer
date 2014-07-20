cce99179058039d74ed6e1913c53fffef144f347
OpenShift guest <rzepkafoto@gmail.com>
2014-07-20 09:54:44 -0400

    Termometer aplication

---
+++ README.md
@@ -0,0 +1,3 @@
+Termometer aplication wich generates web sites with cool looking graph :smile:
+Works on rasppbery pi using DS18B20. 
+Generates line charts by chart.js.
---
+++ tocsv.py
@@ -0,0 +1,24 @@
+import time
+import csv
+
+while True:
+    with open("/sys/bus/w1/devices/28-000003ead460/w1_slave","r") as f:
+        tem=f.read()
+    
+    tem = tem.split("\n")
+    tem = tem[1]
+    tem = tem.split(" ")
+    tem = tem[9]
+    tem = tem[2:]
+    tem=float(tem)/1000
+    
+    tim= time.localtime()
+    tim=time.mktime(tim)
+    tim= time.strftime("%d.%m %H:%M " )
+    with open('data.csv','a') as csvf:
+        csvw=csv.writer(csvf, delimiter=' ',quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        csvw.writerow([tem,tim])
+    tem="{0:.3f}".format(tem)
+    time.sleep(60)
+
+
